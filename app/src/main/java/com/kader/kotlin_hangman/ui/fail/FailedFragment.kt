1ef6bba0bab62d1f60a9daff4eb5d91607a4b0dc
kadersutlu <sutlukader@gmail.com>
2024-05-05 23:19:37 +0300

    update fail and success

--- app/src/main/java/com/kader/kotlin_hangman/ui/fail/FailedFragment.kt
+++ app/src/main/java/com/kader/kotlin_hangman/ui/fail/FailedFragment.kt
@@ -2,9 +2,12 @@ package com.kader.kotlin_hangman.ui.fail
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.viewModels
+import com.kader.kotlin_hangman.R
 import com.kader.kotlin_hangman.databinding.FragmentFailedBinding
 import com.kader.kotlin_hangman.ui.BaseFragment
+import com.kader.kotlin_hangman.ui.game.GameFragment
 import com.kader.kotlin_hangman.util.ScreenName
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -12,6 +15,18 @@ import dagger.hilt.android.AndroidEntryPoint
 class FailedFragment (override val screenName: String = ScreenName.FAILED_SCREEN) :
     BaseFragment<FragmentFailedBinding, FailedViewModel>() {
     override fun initView() {
+
+        val correctWord = arguments?.getString("correctWord") ?: ""
+
+        binding.correctWord.text = "Doğru cevap: $correctWord"
+
+        binding.buttonAgain.setOnClickListener{
+            val gameFragment = GameFragment()
+            val transaction = requireActivity().supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.fragment_container, gameFragment)
+            transaction.addToBackStack(null)
+            transaction.commit()
+        }
 
     }
 
--- app/src/main/java/com/kader/kotlin_hangman/ui/success/SuccessFragment.kt
+++ app/src/main/java/com/kader/kotlin_hangman/ui/success/SuccessFragment.kt
@@ -3,8 +3,10 @@ package com.kader.kotlin_hangman.ui.success
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import com.kader.kotlin_hangman.R
 import com.kader.kotlin_hangman.databinding.FragmentSuccessBinding
 import com.kader.kotlin_hangman.ui.BaseFragment
+import com.kader.kotlin_hangman.ui.game.GameFragment
 import com.kader.kotlin_hangman.util.ScreenName
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -12,7 +14,13 @@ import dagger.hilt.android.AndroidEntryPoint
 class SuccessFragment(override val screenName: String = ScreenName.SUCCESS_SCREEN) :
     BaseFragment<FragmentSuccessBinding, SuccessViewModel>() {
     override fun initView() {
-
+        binding.buttonSucces.setOnClickListener {
+            val gameFragment = GameFragment()
+            val transaction = requireActivity().supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.fragment_container, gameFragment)
+            transaction.addToBackStack(null)
+            transaction.commit()
+        }
     }
 
     override val viewModel by viewModels<SuccessViewModel>()
