80e0fca20e614766d85a75bdf1b37b81a5c254c6
kadersutlu <sutlukader@gmail.com>
2024-05-17 22:28:29 +0300

    fixed room db

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,23 +52,26 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-    //Navigation Component:
+    //Navigation Component
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+
     //lifecycle
     val lifecycle_version = "2.6.2"
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle_version")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycle_version")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycle_version")
+
     //hilt
     implementation("com.google.dagger:hilt-android:2.50")
     kapt("com.google.dagger:hilt-android-compiler:2.50")
+
     //room
     val room_version = "2.6.1"
     implementation("androidx.room:room-runtime:$room_version")
     annotationProcessor("androidx.room:room-compiler:$room_version")
-    implementation("androidx.room:room-ktx:$room_version")
-    annotationProcessor("android.arch.persistence.room:compiler:$room_version")
+    kapt("androidx.room:room-compiler:$room_version")
+
     //splash-screen
     implementation("androidx.core:core-splashscreen:1.0.1")
 
